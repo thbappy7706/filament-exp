d86b9b7be430f7d1acf1c67354be6334037fb7ae
tanvir.bappy <tanvir.bappy@bansberrysg.com>
2025-07-15 15:39:39 +0600

    global search

--- app/Filament/Resources/ProductResource.php
+++ app/Filament/Resources/ProductResource.php
@@ -4,7 +4,6 @@ namespace App\Filament\Resources;
 
 use App\Enums\ProductTypeEnum;
 use App\Filament\Resources\ProductResource\Pages;
-use App\Filament\Resources\ProductResource\RelationManagers;
 use App\Models\Product;
 use Filament\Actions\ActionGroup;
 use Filament\Forms;
@@ -14,6 +13,7 @@ use Filament\Resources\Resource;
 use Filament\Tables;
 use Filament\Tables\Table;
 use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletingScope;
 use Filament\Forms\Set;
 use Illuminate\Support\Str;
@@ -26,37 +26,57 @@ class ProductResource extends Resource
     protected static ?int $navigationSort = 0;
     protected static ?string $navigationGroup = 'Shop';
 
+    protected static ?string $recordTitleAttribute = 'name';
+    protected static int $globalSearchResultsLimit = 20;
+
+
+    public static function getGloballySearchableAttributes(): array
+    {
+        return  ['name','slug','description'];
+    }
+
+    public static function getGlobalSearchResultDetails(Model $record): array
+    {
+        return [
+          'Brand' => $record->brand->name,
+        ];
+    }
+
+    public static function getGlobalSearchEloquentQuery(): Builder
+    {
+        return parent::getGlobalSearchEloquentQuery()->with(['brand']);
+    }
+
     public static function form(Form $form): Form
     {
         return $form
             ->schema([
-               Forms\Components\Group::make()->schema([
-                   Forms\Components\Section::make('Basic Information')->schema([
-                       Forms\Components\TextInput::make('name')->required()
-                           ->live(onBlur:  true)->unique()
-                           ->afterStateUpdated(fn (Set $set, ?string $state) => $set('slug', Str::slug($state))),
-                       Forms\Components\TextInput::make('slug')->disabled()->dehydrated()->unique(Product::class,'slug',ignoreRecord: true),
-                       Forms\Components\MarkdownEditor::make('description')->columnSpan('full'),
+                Forms\Components\Group::make()->schema([
+                    Forms\Components\Section::make('Basic Information')->schema([
+                        Forms\Components\TextInput::make('name')->required()
+                            ->live(onBlur: true)->unique()
+                            ->afterStateUpdated(fn(Set $set, ?string $state) => $set('slug', Str::slug($state))),
+                        Forms\Components\TextInput::make('slug')->disabled()->dehydrated()->unique(Product::class, 'slug', ignoreRecord: true),
+                        Forms\Components\MarkdownEditor::make('description')->columnSpan('full'),
 
-                   ])->columns(2),
+                    ])->columns(2),
 
 
-                   Forms\Components\Section::make('Pricing & Inventory')->schema([
-                       Forms\Components\TextInput::make('sku')->label("SKU (Stock Keeping Unit)")->required()->unique(),
-                       Forms\Components\TextInput::make('price')
-                           ->numeric()->rules([
-                           'regex:/^\d+(\.\d{1,2})?$/'
-                       ])->required(),
-                       Forms\Components\TextInput::make('quantity')->numeric()->minValue(0)->maxValue(100)->required(),
-                       Forms\Components\Select::make('type')->options([
-                           'downloadable' => ProductTypeEnum::DOWNLOADABLE->value,
-                           'deliverable' => ProductTypeEnum::DELIVERABLE->value,
-                       ])->required(),
+                    Forms\Components\Section::make('Pricing & Inventory')->schema([
+                        Forms\Components\TextInput::make('sku')->label("SKU (Stock Keeping Unit)")->required()->unique(),
+                        Forms\Components\TextInput::make('price')
+                            ->numeric()->rules([
+                                'regex:/^\d+(\.\d{1,2})?$/'
+                            ])->required(),
+                        Forms\Components\TextInput::make('quantity')->numeric()->minValue(0)->maxValue(100)->required(),
+                        Forms\Components\Select::make('type')->options([
+                            'downloadable' => ProductTypeEnum::DOWNLOADABLE->value,
+                            'deliverable' => ProductTypeEnum::DELIVERABLE->value,
+                        ])->required(),
 
-                   ])->columns(2)
+                    ])->columns(2)
 
-               ]),
-
+                ]),
 
 
                 Forms\Components\Group::make()->schema([
@@ -64,14 +84,14 @@ class ProductResource extends Resource
                         Forms\Components\Toggle::make('is_visible')
                             ->label('Visibility')->helperText('Enable or disable product visibility.')->default(true),
                         Forms\Components\Toggle::make('is_featured')
-                                ->label('Featured')->helperText('Enable or disable product featured.')->default(true),
+                            ->label('Featured')->helperText('Enable or disable product featured.')->default(true),
 
                         DatePicker::make('published_at')->label('Availability')->default(now()),
                     ]),
                     Forms\Components\Section::make('Image')->schema([
                         Forms\Components\FileUpload::make('image')
                             ->directory('product-attachments')->preserveFilenames()
-                             ->image()->imageEditor(),
+                            ->image()->imageEditor(),
 
                     ])->collapsible(),
 
@@ -82,7 +102,6 @@ class ProductResource extends Resource
                     ]),
 
                 ]),
-
 
 
             ]);
@@ -108,10 +127,10 @@ class ProductResource extends Resource
             ])
             ->actions([
                 Tables\Actions\ActionGroup::make([
-                   Tables\Actions\ViewAction::make(),
-                   Tables\Actions\EditAction::make(),
-                   Tables\Actions\DeleteAction::make(),
-               ])
+                    Tables\Actions\ViewAction::make(),
+                    Tables\Actions\EditAction::make(),
+                    Tables\Actions\DeleteAction::make(),
+                ])
             ])
             ->bulkActions([
                 Tables\Actions\BulkActionGroup::make([
